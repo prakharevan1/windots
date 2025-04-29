18201d5b8e7922837e5c91d81e1d19eb45f0ab88
Evan Dagur <dagurevan@gmail.com>
2025-04-29 08:26:58 -0700

    Rust now actually works for windows!

--- Appdata/local/nvim/lua/freerunning/code_formatting/rustaceanvim.lua
+++ Appdata/local/nvim/lua/freerunning/code_formatting/rustaceanvim.lua
@@ -2,74 +2,49 @@ return {
     {
         "mrcjkb/rustaceanvim",
         version = "^5", -- Recommended
-        lazy = false, -- This plugin is already lazy
+        lazy = false,   -- This plugin is already lazy
         config = function()
-            -- cargo run
-            vim.keymap.set(
-                "n",
-                "<leader>rr",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo run'} })<CR>",
+            local function win_cmd(cmd)
+                local dir = vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p')
+                -- Escape backslashes and wrap the full command in quotes
+                return { 'cmd.exe', '/C', 'cd /D "' .. dir .. '" && ' .. cmd }
+            end
+
+            vim.keymap.set("n", "<leader>rr",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo run') }) end,
                 { noremap = true, silent = true, desc = "Run rust code with cargo (debug)" }
             )
-            -- cargo run (release)
-            vim.keymap.set(
-                "n",
-                "<leader>rR",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo run --release'} })<CR>",
+            vim.keymap.set("n", "<leader>rR",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo run --release') }) end,
                 { noremap = true, silent = true, desc = "Run rust code with cargo (release)" }
             )
-            -- cargo test
-            vim.keymap.set(
-                "n",
-                "<leader>rt",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo test'} })<CR>",
+            vim.keymap.set("n", "<leader>rt",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo test') }) end,
                 { noremap = true, silent = true, desc = "Test rust code with cargo" }
             )
-            -- cargo build
-            vim.keymap.set(
-                "n",
-                "<leader>rb",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo build'} })<CR>",
+            vim.keymap.set("n", "<leader>rb",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo build') }) end,
                 { noremap = true, silent = true, desc = "Build rust code with cargo (debug)" }
             )
-            -- cargo build (release)
-            vim.keymap.set(
-                "n",
-                "<leader>rB",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo build --release'} })<CR>",
+            vim.keymap.set("n", "<leader>rB",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo build --release') }) end,
                 { noremap = true, silent = true, desc = "Build rust code with cargo (release)" }
             )
-            -- cargo check
-            vim.keymap.set(
-                "n",
-                "<leader>rc",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo check'} })<CR>",
+            vim.keymap.set("n", "<leader>rc",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo check') }) end,
                 { noremap = true, silent = true, desc = "Check rust code for errors with cargo" }
             )
-            -- cargo clippy (linting)
-            vim.keymap.set(
-                "n",
-                "<leader>rl",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo clippy'} })<CR>",
-                {
-                    noremap = true,
-                    silent = true,
-                    desc = "Use cargo clippy to provide suggestions and warnings for errors in your rust code",
-                }
+            vim.keymap.set("n", "<leader>rl",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo clippy') }) end,
+                { noremap = true, silent = true, desc = "Lint rust code with cargo clippy" }
             )
-            -- rust docs
-            vim.keymap.set(
-                "n",
-                "<leader>rd",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo doc --open'} })<CR>",
-                { noremap = true, silent = true, desc = "Open the rust docs with cargo to read up on documentation" }
+            vim.keymap.set("n", "<leader>rd",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo doc --open') }) end,
+                { noremap = true, silent = true, desc = "Open rust docs with cargo" }
             )
-            -- cargo clean
-            vim.keymap.set(
-                "n",
-                "<leader>ra",
-                ":lua require('FTerm').scratch({ cmd = {'bash', '-c', 'cd ' .. vim.fn.fnamemodify(vim.fn.expand('%:p:h:h'), ':p') .. ' && cargo clean'} })<CR>",
-                { noremap = true, silent = true, desc = "Clean up all of your built rust programs with cargo clean" }
+            vim.keymap.set("n", "<leader>ra",
+                function() require('FTerm').scratch({ cmd = win_cmd('cargo clean') }) end,
+                { noremap = true, silent = true, desc = "Clean rust build artifacts with cargo clean" }
             )
         end,
     },
